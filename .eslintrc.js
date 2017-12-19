e49d69f59e61518354ccf9cb7ea92cb39e2d6bc1
Kevin Norris <kevin.norris.90@gmail.com>
2017-12-19 09:19:57 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/no-array-index-key': 'off',
+    'arrow-parens': 'off',
+    'import/no-named-as-default': 'off',
+  },
+};
